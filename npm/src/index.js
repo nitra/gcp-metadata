9079c58a497b64d05c77a84523da8e2e6f93de49
vitaliytv <vitaliytv@nitralabs.com>
2023-05-03 15:20:53 +0300

    abinbevefes/caps#392

--- npm/src/index.js
+++ npm/src/index.js
@@ -2,28 +2,36 @@ import gcpMetadata from 'gcp-metadata'
 
 let projectId
 export async function getProjectId() {
-  // If a user is instantiating several call at the same time,
-  // this may result in multiple calls to getProjectId(), to detect the
-  // runtime environment. We use the same promise for each of these calls
-  // to reduce the network load.
-  if (projectId === undefined && (await gcpMetadata.isAvailable())) {
-    projectId = gcpMetadata.project('project-id')
-  }
+  try {
+    // If a user is instantiating several call at the same time,
+    // this may result in multiple calls to getProjectId(), to detect the
+    // runtime environment. We use the same promise for each of these calls
+    // to reduce the network load.
+    if (projectId === undefined && (await gcpMetadata.isAvailable())) {
+      projectId = gcpMetadata.project('project-id')
+    }
 
-  return projectId
+    return projectId
+  } catch (e) {
+    return process.env.GCP_PROJECT
+  }
 }
 
 let region
 export async function getRegion() {
-  // If a user is instantiating several call at the same time,
-  // this may result in multiple calls to getRegion(), to detect the
-  // runtime environment. We use the same promise for each of these calls
-  // to reduce the network load.
-  if (region === undefined && (await gcpMetadata.isAvailable())) {
-    region = gcpMetadata.project('attributes/google-compute-default-region')
-  }
+  try {
+    // If a user is instantiating several call at the same time,
+    // this may result in multiple calls to getRegion(), to detect the
+    // runtime environment. We use the same promise for each of these calls
+    // to reduce the network load.
+    if (region === undefined && (await gcpMetadata.isAvailable())) {
+      region = gcpMetadata.project('attributes/google-compute-default-region')
+    }
 
-  return region
+    return region
+  } catch (e) {
+    return process.env.GCP_REGION
+  }
 }
 
 /**
