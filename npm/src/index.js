8e48d7e189ed3733a78fc6540e45dbf88ef9ed8f
vitaliytv <vitaliytv@nitralabs.com>
2023-05-05 11:26:12 +0300

    GCP_PROJECT GCP_REGION priority

--- npm/src/index.js
+++ npm/src/index.js
@@ -2,36 +2,35 @@ import gcpMetadata from 'gcp-metadata'
 
 let projectId
 export async function getProjectId() {
-  try {
-    // If a user is instantiating several call at the same time,
-    // this may result in multiple calls to getProjectId(), to detect the
-    // runtime environment. We use the same promise for each of these calls
-    // to reduce the network load.
-    if (projectId === undefined && (await gcpMetadata.isAvailable())) {
-      projectId = gcpMetadata.project('project-id')
-    }
-
-    return projectId || process.env.GCP_PROJECT
-  } catch (e) {
+  if (process.env.GCP_PROJECT) {
     return process.env.GCP_PROJECT
   }
+  // If a user is instantiating several call at the same time,
+  // this may result in multiple calls to getProjectId(), to detect the
+  // runtime environment. We use the same promise for each of these calls
+  // to reduce the network load.
+  if (projectId === undefined && (await gcpMetadata.isAvailable())) {
+    projectId = gcpMetadata.project('project-id')
+  }
+
+  return projectId
 }
 
 let region
 export async function getRegion() {
-  try {
-    // If a user is instantiating several call at the same time,
-    // this may result in multiple calls to getRegion(), to detect the
-    // runtime environment. We use the same promise for each of these calls
-    // to reduce the network load.
-    if (region === undefined && (await gcpMetadata.isAvailable())) {
-      region = gcpMetadata.project('attributes/google-compute-default-region')
-    }
-
-    return region || process.env.GCP_REGION
-  } catch (e) {
+  if (process.env.GCP_REGION) {
     return process.env.GCP_REGION
   }
+
+  // If a user is instantiating several call at the same time,
+  // this may result in multiple calls to getRegion(), to detect the
+  // runtime environment. We use the same promise for each of these calls
+  // to reduce the network load.
+  if (region === undefined && (await gcpMetadata.isAvailable())) {
+    region = gcpMetadata.project('attributes/google-compute-default-region')
+  }
+
+  return region
 }
 
 /**
